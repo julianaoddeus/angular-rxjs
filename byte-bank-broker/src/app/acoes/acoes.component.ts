ad624bd47b476aa8034394aea1d5bba2874de658
Juliana <julianaacademi@gmail.com>
2022-06-07 15:56:06 -0300

    projeto finalizado

--- byte-bank-broker/src/app/acoes/acoes.component.ts
+++ byte-bank-broker/src/app/acoes/acoes.component.ts
@@ -1,10 +1,11 @@
 import { Component} from '@angular/core';
 import { FormControl } from '@angular/forms';
-import {switchMap, tap} from 'rxjs/operators'
 import {merge} from 'rxjs'
+import {debounceTime, distinctUntilChanged, filter, switchMap, tap} from 'rxjs/operators'
 
 import { AcoesService } from './acoes.service';
 
+const ESPERA_DIGITACAO = 300;
 
 @Component({
   selector: 'app-acoes',
@@ -16,7 +17,11 @@ export class AcoesComponent {
   acoesInput = new FormControl();
   todasAcoes$ =  this.acoesService.getAcoes().pipe(tap(() => {console.log('Fluxo inicial')}));
   filtroPeloInput$ =  this.acoesInput.valueChanges.pipe(
+    debounceTime(ESPERA_DIGITACAO) ,
     tap(()=> {console.log('Fluxo do Filtro')}),
+    tap(console.log),
+    filter((valorDigitado) => valorDigitado.length >= 3 || !valorDigitado.length),
+    distinctUntilChanged(),
     switchMap((valorDigitado)=> this.acoesService.getAcoes(valorDigitado))
   )
   acoes$ = merge(this.todasAcoes$, this.filtroPeloInput$)
